0feb93c3f3c600deadee913ae101e95f410997d8
malvinamaria <mmpolak@protonmail.com>
2023-05-15 13:33:28 +0200

    adding smth

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import data from './data.json';
+
+// console.log(data.length);
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -10,10 +13,31 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+// app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Start defining your routes here ENDPOINT
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+app.get('/nominations', (req, res) => {
+  res.json(data);
+});
+
+app.get('/year/:year', (req, res) => {
+  const year = req.params.year;
+  const showWon = req.query.won;
+  console.log({ showWon });
+  // console.log({ year });
+  // changing from const to let to filter second time
+  let nominationsFromYear = data.filter((item) => item.year_award === +year);
+
+  // showing only won movies with ?won=true
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win);
+  }
+
+  res.json(nominationsFromYear);
 });
 
 // Start the server
